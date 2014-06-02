73e574d116cf96fccdaf3fa2eb25975d31c1d77f
ryood <o.d.r.ryo@gmail.com>
2014-06-03 03:24:06 +0900

    bat ファイルの追加

---
+++ DDSTest.bat
@@ -0,0 +1,5 @@
+rem DDSTest-12bitTable.exe > DDSTest-12bitTable.raw
+rem D:\home\app\sox12177\sox.exe -s -w -r 44100 -c 1 "DDSTest-12bitTable.raw" "DDSTest-12bitTable.wav"
+
+DDSTest-12bitTable\DDSTest16bit-12bitTable.exe > DDSTest16bit-12bitTable.raw
+D:\home\app\sox12177\sox.exe -s -w -r 44100 -c 1 "DDSTest16bit-12bitTable.raw" "DDSTest16bit-12bitTable.wav"
