635834a6258010e7add8d3310db941957ad2f1f7
ryood <o.d.r.ryo@gmail.com>
2014-06-14 00:01:03 +0900

    プロジェクト DDSTest-LFO を追加

---
+++ DDSTest-LFO/DDSTest-LFO.cpp
@@ -0,0 +1,93 @@
+// DDSTest-LFO.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+/*********************************************************
+phaseRegister   : 16bit
+tunigWord       : 16bit
+lookupTable     : 16bit (use 12bit)
+
+Phase Modulation LFO
+lfoAmount		: 8bit
+**********************************************************/
+
+#define SAMPLE_CLOCK	44100	// 44.1kHz
+#define TABLE_SIZE	    0x0400  // Lookup Table Size = 1024
+
+uint16_t *lookupTable;
+
+uint16_t phaseRegister;
+uint16_t tuningWord;
+int16_t  waveValue;
+
+uint16_t lfoPhaseRegister;
+uint16_t lfoTuningWord;
+uint8_t  lfoAmount = 255;
+
+// frequency > SAMPLE_CLOCK / 2^16 (about 0.67Hz)
+double frequency = 440.0;
+double lfoFrequency = 1.5;
+
+int period = 30 * SAMPLE_CLOCK;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	_setmode(_fileno(stdout), _O_BINARY);
+
+	// Lookup Table の作成
+	lookupTable = (uint16_t*)malloc(TABLE_SIZE * sizeof(uint16_t));
+
+	// Sine Wave
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		lookupTable[i] = (sin(2.0 * M_PI * i / TABLE_SIZE) / 2 + 0.5) * 0x0FFF;
+		//printf("%d\n", lookupTable[i]);
+	}
+	//printf("\n*********************\n\n");
+
+	lfoTuningWord = lfoFrequency * pow(2.0, 16) / SAMPLE_CLOCK;
+	//printf("lfoTuningWord: %d\n", lfoTuningWord);
+
+	tuningWord = frequency * pow(2.0, 16) / SAMPLE_CLOCK;
+	//printf("tuningWord: %d\n", tuningWord);
+
+	phaseRegister = 0x0000;
+	lfoPhaseRegister = 0x0000;
+	for (int i = 0; i < period; i++) {
+		uint16_t index;
+		uint16_t lfoIndex;
+		int16_t  lfoValue;
+		int16_t  waveValue;
+
+		lfoPhaseRegister += lfoTuningWord;
+		//printf("lfoPhaseRegister: %d\n", lfoPhaseRegister);
+
+		lfoIndex = lfoPhaseRegister >> 6;
+		//printf("lfoIndex: %d\n", lfoIndex);
+
+		// lookupTable(12bit) * lfoAmount(8bit) : 20bit -> 16bit
+		lfoValue = (((int32_t)lookupTable[lfoIndex]) - 2048) * lfoAmount >> 4;
+		//printf("lfoValue: %d ->\t", lfoValue);
+
+		// tuningWord(16bit) * lfoValue(16bit) : 32bit -> 16bit
+		lfoValue = (((int32_t)tuningWord) * lfoValue) >> 16;
+		//printf("%d\n", lfoValue);
+
+		phaseRegister += tuningWord + lfoValue;
+		//printf("phaseRegister: %d\n", phaseRegister);
+
+		index = phaseRegister >> 6;
+		//printf("index: %d\n", index);
+
+		waveValue = (lookupTable[index] << 4) - 0x8000;
+		//printf("%d\n", waveValue);
+
+		// 16bit長の raw データとして出力
+		fwrite(&waveValue, sizeof(waveValue), 1, stdout);
+	}
+
+	free(lookupTable);
+
+	return 0;
+}
+
---
+++ DDSTest-LFO/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください。
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <io.h>
+#include <fcntl.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
